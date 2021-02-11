ff415d10372684ca6daf1239a67f11bbd034f973
BCovre <78884340+BCovre@users.noreply.github.com>
2021-02-10 22:24:43 -0300

    Add files via upload

---
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package meuPrimeiroProgramaJava;
+
+/**
+ * @author bcovr
+ *
+ */
+public class Principal {
+	public static void main(String[] args) {
+		
+		//Saldação e pergunta o nome
+		System.out.println("Olá amigo!\nQual é o seu nome?");
+
+		// Ler o nome
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+				
+		// Saldação específica para o nome lido
+		System.out.printf("Olá %s!", nome);
+				
+		//Nota: Implementar leitura de idade e exibi-la
+	}
+
+}
